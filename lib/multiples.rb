baac57b4de09dbcd891a6302bc11c8824f758d8d
Lia Skalkos <hello@liaskalkos.net>
2015-07-01 09:29:12 -0400

    Complete

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,37 @@
 # Enter your procedural solution here!
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+# n=0
+# sum=0
+#
+# while n < 1000
+#   if (n % 3 == 0)
+#     sum += n
+#   elsif (n % 5 ==0)
+#     sum += n
+#   end
+#
+#   n+=1
+# end
+#
+# puts sum
+
+def collect_multiples(limit)
+  array = []
+  n = 1
+  while n < limit
+    if (n % 3 == 0)
+      array << n
+    elsif (n % 5 ==0)
+      array << n
+    end
+
+    n+=1
+  end
+  array
+end
+
+def sum_multiples(limit)
+ collect_multiples(limit).inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,29 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    array = []
+    n = 1
+    while n < @limit
+      if (n % 3 == 0)
+        array << n
+      elsif (n % 5 ==0)
+        array << n
+      end
+
+      n+=1
+    end
+    array
+  end
+
+  def sum_multiples
+    collect_multiples.inject(:+)
+  end
+
+
+end
